136bf2f2db8087699c50ca79e39e92723dda6c26
Aditya <adityaraj6311@gmail.com>
2022-06-14 18:50:45 +0530

    k

--- settings.js
+++ settings.js
@@ -19,20 +19,20 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['1 (786) 233-0930']
+global.premium = ['1 (786) 233-0930']
+global.ownernomer = '1 (786) 233-0930'
+global.ownername = 'Tofu'
+global.botname = 'Zero-Two'
 global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
+global.ig = 'https://github.com/Awesome-Tofu'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
 global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.author = 'Tofu'
 global.sessionName = 'session'
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','❤️','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
