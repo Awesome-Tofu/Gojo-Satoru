ec99efb46755b07cba11dfcdf4b58f133ef1404d
Aditya <adityaraj6311@gmail.com>
2022-07-09 19:09:38 +0530

    ee

--- Gojosensei.js
+++ Gojosensei.js
@@ -141,7 +141,7 @@ module.exports = GojoMdNx = async (GojoMdNx, m, chatUpdate, store) => {
         const text = q = args.join(" ")
         const quoted = m.quoted ? m.quoted : m
         const mime = (quoted.msg || quoted).mimetype || ''
-	    const isMedia = /image|video|sticker|audio/.test(mime)
+	    const isMedia = /image|video|sticker|gif|audio/.test(mime)
 	    const from = mek.key.remoteJid
 	    const type = Object.keys(mek.message)[0]        
 	    const content = JSON.stringify(mek.message)
