b9c4cefd58909599f293b46d8494e06c275922e9
Aditya <adityaraj6311@gmail.com>
2022-07-20 16:02:26 +0530

    🟡

--- Gojosensei.js
+++ Gojosensei.js
@@ -111,6 +111,7 @@ let setik = JSON.parse(fs.readFileSync('./database/setik.json'));
 let vien = JSON.parse(fs.readFileSync('./database/vien.json'));
 let imagi = JSON.parse(fs.readFileSync('./database/imagi.json'))
 let videox = JSON.parse(fs.readFileSync('./database/video.json'))
+let gifx = JSON.parse(fs.readFileSync('./database/gif.json'))
 
 //read database\\
 let tebaklagu = db.data.game.tebaklagu = []
@@ -140,7 +141,7 @@ module.exports = GojoMdNx = async (GojoMdNx, m, chatUpdate, store) => {
         const text = q = args.join(" ")
         const quoted = m.quoted ? m.quoted : m
         const mime = (quoted.msg || quoted).mimetype || ''
-	    const isMedia = /image|video|sticker|audio/.test(mime)
+	    const isMedia = /image|video|sticker|gif|audio/.test(mime)
 	    const from = mek.key.remoteJid
 	    const type = Object.keys(mek.message)[0]        
 	    const content = JSON.stringify(mek.message)
@@ -280,6 +281,12 @@ const reply = (teks) => {
 					GojoMdNx.sendMessage(m.chat, { video: result }, { quoted: m })
 					}
                 }
+                for (let anjh of gifx){
+                    if (budy === anjh){
+                        result = fs.readFileSync(`./GojoMedia/gif/${anjh}.gif`)
+                        GojoMdNx.sendMessage(m.chat, { video: result, gifPlayback: true }, { quoted: m })
+                        }
+                    }
 				  
 
       //Mute Chat\\
