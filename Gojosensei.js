53474fb9e51fcc7450e153ed44cc894b714af228
Aditya <adityaraj6311@gmail.com>
2022-07-24 12:25:25 +0530

    chatbot

--- Gojosensei.js
+++ Gojosensei.js
@@ -2064,6 +2064,26 @@ break
                 })
                 }
                 break
+                case 'chatbot': {
+                    if (!text) return reply(`Example 1 : ${prefix + command} on\nExmaple 2 : ${prefix + command} off`)
+                    if (!m.isGroup) return replay(`${mess.group}`)
+                    if (args[0] === "on") {
+                        if (db.data.chats[m.chat].chatbot) return reply(`Previously Active`)
+                        db.data.chats[m.chat].chatbot = true
+                        reply(`${GojoMdNx.user.name} Has Turned ON Chatbot !`)
+                    } else if (args[0] === "off") {
+                        if (!db.data.chats[m.chat].chatbot) return reply(`Previously Inactive`)
+                        db.data.chats[m.chat].chatbot = false
+                        reply(`${GojoMdNx.user.name} Has Turned OFF Chatbot !`)
+                        } else {
+                            let buttons = [
+                                { buttonId: 'chatbot on', buttonText: { displayText: 'On' }, type: 1 },
+                                { buttonId: 'chatbot off', buttonText: { displayText: 'Off' }, type: 1 }
+                            ]
+                            await GojoMdNx.sendButtonText(m.chat, buttons, `Chatbot`, GojoMdNx.user.name, m)
+                        }
+                     }
+                     break
         case 'gimage': case 'googleimage': {
         if (!text) return reply(`Example : ${prefix + command} gojo`)
         let gis = require('g-i-s')
