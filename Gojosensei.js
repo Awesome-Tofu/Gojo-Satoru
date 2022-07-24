1aaf725d9b6e30032800427b34bf470dbb811af8
Aditya <adityaraj6311@gmail.com>
2022-07-24 10:19:29 +0530

    🍊

--- Gojosensei.js
+++ Gojosensei.js
@@ -2679,7 +2679,7 @@ case 'webtonsearch': case 'webtoon':
                     if (!id) return reply(`No Query username, Example : ${prefix + command} ig aditya.agatsuma`)
                     let anu = await fetchJson(api('zenz', '/api/stalker/ig', { username: id }, 'apikey'))
                     if (anu.status == false) return reply(anu.result.message)
-                    GojoMdNx.sendMedia(m.chat, anu.profile, '', `❤️ Full Name : ${anu.fullname}\n❤️ User Name : ${anu.caption.username}\n❤️ Following : ${anu.caption.followeing}\n❤️ Followers : ${anu.caption.followers}\n❤️ Post : ${anu.caption.post}\n❤️ Bio : ${anu.caption.biography}`, m)
+                    GojoMdNx.sendMedia(m.chat, anu.profile, '', `❤️ Full Name : ${anu.fullname}\n❤️ User Name : ${anu.username}\n❤️ Following : ${anu.followeing}\n❤️ Followers : ${anu.followers}\n❤️ Post : ${anu.post}\n❤️ Bio : ${anu.biography}`, m)
 		    db.data.users[m.sender].limit -= 1
                 } else if (type.toLowerCase() == 'npm') {
                     if (!id) return reply(`No Query username, Example : ${prefix + command} npm scrape-primbon`)
